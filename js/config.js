be8c807cf95ac2e628fc579d4a97afce7519ad9f
vitor221 <vitornascimento6650@gmail.com>
2023-01-03 21:54:17 -0300

    modificacao de estrutura logica

--- js/config.js
+++ js/config.js
@@ -1,33 +1,69 @@
-var btns = document.querySelectorAll('.btns')
+const select = document.getElementById('areaBtn')
 
-for(var i = 0; i < btns.length; i++){
-  btns[i].addEventListener("click", function() {
-    var select = document.getElementsByClassName("ativo");
+let btnPrev = null
 
-    if(select.length > 0){
-      select[0].className = select[0].className.replace("ativo", "");
-    }
+select.addEventListener('click', (e) => {
+  const btnValidate = e.target.nodeName === 'BUTTON'; //Verificar se é um botão
+  if(!btnValidate){
+    return;
+  }
 
-    this.className += " ativo";
-  })
+  e.target.classList.add('ativo');
+ 
+
+  if(btnPrev != null) {
+    btnPrev.classList.remove('ativo');
+  }
+
+  btnPrev = e.target;
+})
+
+function btnEnv1(){
+  var enviarValue = document.getElementById('btnSubmit').value = 1
+  console.log(enviarValue)
 }
 
-var btn = document.getElementsByClassName('ativo')
-var submit = document.getElementById('btnSubmit')
-var btnValue1 = document.getElementById('btnVal1')
-console.log(btnValue1)
-var btnValue2 = document.getElementById('btnVal2').value
-console.log(btnValue2)
+function btnEnv2() {
+  var enviarValue = document.getElementById('btnSubmit').value = 2
+  console.log(enviarValue)
+}
 
-submit.addEventListener('click', function (e) {
-  var campo = document.getElementById('btnVal1').value
+function btnEnv3() {
+  var enviarValue = document.getElementById('btnSubmit').value = 3
+  console.log(enviarValue)
+}
 
-  if(campo == 1 && btnValue1 == 1){
-    window.location.href = 'thank1.html';
-  } else {
-    alert('error')
+function btnEnv4() {
+  var enviarValue = document.getElementById('btnSubmit').value = 4
+  console.log(enviarValue)
+}
+
+function btnEnv5() {
+  var enviarValue = document.getElementById('btnSubmit').value = 5
+  console.log(enviarValue)
+}
+
+function enviar(){
+  var enviarNull = document.getElementById('btnSubmit').value
+
+
+  if(enviarNull == 1){
+    window.location.href = 'thank1.html'
   }
-})
-  
-  
 
+  if(enviarNull == 2) {
+    window.location.href = 'thank2.html'
+  }
+
+  if(enviarNull == 3){
+    window.location.href = 'thank3.html'
+  }
+
+  if(enviarNull == 4){
+    window.location.href = 'thank4.html'
+  }
+
+  if(enviarNull == 5){
+    window.location.href = 'thank5.html'
+  }
+}
--- js/config2.js
+++
@@ -1,5 +0,0 @@
-var res = document.getElementById('res')
-
-
-
-res.innerHTML += `Você selecionou ${btnValue1} de 5`
